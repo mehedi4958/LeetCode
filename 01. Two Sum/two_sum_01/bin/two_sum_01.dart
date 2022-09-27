37ccf5867e668b37c42cb59ae54aab52e20a324b
Mehedi <mehedi@augnitive.com>
2022-09-27 07:33:07 +0600

    LeetCode 1. Two Sum Accepted

---
+++ 01. Two Sum/two_sum_01/bin/two_sum_01.dart
@@ -0,0 +1,15 @@
+void main() {
+ final nums = <int>[3,2,3,1];
+  final target = 6;
+}
+
+List<int> twoSum(List<int> nums, int target) {
+    for (int i = 0; i < nums.length - 1; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          return [i, j];
+        }
+      }
+    }
+  return [];
+}
---
+++ 01. Two Sum/two_sum_01/test/two_sum_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:two_sum_01/two_sum_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
